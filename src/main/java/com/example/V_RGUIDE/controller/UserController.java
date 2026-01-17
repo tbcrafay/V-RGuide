bc24ee37b8d551e06baf220c8f006cc8265f903f
tbcrafay <abdulrafaymehmood123@gmail.com>
2026-01-17 19:12:49 +0500

    forntend integeraed only

--- src/main/java/com/example/V_RGUIDE/controller/UserController.java
+++ src/main/java/com/example/V_RGUIDE/controller/UserController.java
@@ -4,6 +4,7 @@ import java.util.List;
 import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -22,6 +23,7 @@ import com.example.V_RGUIDE.service.UserService;
 
 @RestController
 @RequestMapping("/api/users")
+@CrossOrigin(origins = "http://localhost:3000")
 public class UserController {
 
     @Autowired
